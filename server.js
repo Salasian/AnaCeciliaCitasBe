fb9665df94128366e48501bd646e15c9abe9b451
Ian Salas López <96995669+Salasian@users.noreply.github.com>
2024-01-24 15:40:50 -0700

    Start

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://iansalas216755:salon@anaceciliasalon.bvymsg4.mongodb.net/anacecilia",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use("/cita", require("./routes/citaRoute"));
+
+app.use("/servicio", require("./routes/servicioRoute"));
+
+app.use("/cliente", require("./routes/clienteRoute"));
+
+app.use("/producto", require("./routes/productoRoute"));
+
+app.use("/image", require("./routes/imageRoute"));
+
+app.listen(3001, function () {
+  console.log("Running on 3001");
+});
